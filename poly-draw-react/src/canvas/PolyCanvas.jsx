1811ce2c288c66a67ff459150735c2ad10421fa6
Adrian Hansen <adrian.hansen.flensburg@gmail.com>
2025-12-21 21:31:02 +0100

    add draw preview and adjust first line vertex

--- poly-draw-react/src/canvas/PolyCanvas.jsx
+++ poly-draw-react/src/canvas/PolyCanvas.jsx
@@ -1,18 +1,36 @@
 import { Canvas } from "@react-three/fiber";
 import { Line, OrthographicCamera } from "@react-three/drei";
+import { useState } from "react";
 
 export function PolyCanvas() {
-  const points = [
-    [0, 2],
-    [0, 1],
-    [2, 2],
-    [0, 2],
-  ];
+  const [points, setPoints] = useState([]);
+  const [preview, setPreview] = useState([]);
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <Canvas>
+        <mesh
+          position={[0, 0, -1]}
+          onPointerMove={(e) => {
+            const { x, y } = e.point;
+            if (points.length > 0) {
+              setPreview([points[points.length - 1], [x, y]]);
+            }
+          }}
+          onClick={(e) => {
+            const { x, y } = e.point;
+            setPoints((prev) => [...prev, [x, y]]);
+          }}
+        >
+          <planeGeometry args={[100, 100]} />
+          <meshBasicMaterial opacity={0} />
+        </mesh>
         <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={100} />
-        <Line points={points} color="black" lineWidth={1}></Line>
+        {points.length > 1 && (
+          <Line points={points} color="black" lineWidth={2} />
+        )}
+        {preview.length > 0 && (
+          <Line points={preview} color="red" lineWidth={2} />
+        )}
       </Canvas>
     </div>
   );
