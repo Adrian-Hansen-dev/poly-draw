b894788ad7392aade347a07f3238f01b1829eeee
iMarko00 <markomisic75@gmail.com>
2026-01-07 23:57:16 +0100

    fixing dotNet Version to make Feliz run

--- polygon-sketching-project-main/vite.config.js
+++ polygon-sketching-project-main/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fable from "vite-plugin-fable";
 
 export default defineConfig({
   plugins: [
-    react(),
-    fable({ project: "./src/App.fsproj" })
+    react()
   ],
   root: "./src",
   build: {
